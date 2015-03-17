16d5410eac19f9b6cba8f914110ab6be49f4e31e
Manish Singh <mrsingh@ncsu.edu>
2015-03-17 17:29:53 -0400

    Added mainAdmin Class

---
+++ UnivHousingApp/src/com/univhousing/users/ProjectAdmin.java
@@ -0,0 +1,13 @@
+package com.univhousing.users;
+
+public class ProjectAdmin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
