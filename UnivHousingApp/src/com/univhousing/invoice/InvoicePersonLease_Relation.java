082ad3935d1b2b14de2e390277cdd805bbba487d
Manish Singh <mrsingh@ncsu.edu>
2015-03-24 17:29:48 -0400

    modified InvoicePersonLease_Relation.java displayCurrentInvoice method to display current invoice for that person id

--- UnivHousingApp/src/com/univhousing/invoice/InvoicePersonLease_Relation.java
+++ UnivHousingApp/src/com/univhousing/invoice/InvoicePersonLease_Relation.java
@@ -1,9 +1,13 @@
 package com.univhousing.invoice;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
+import com.univhousing.main.ConnectionUtils;
 
 public class InvoicePersonLease_Relation {
 
@@ -73,7 +77,114 @@ public class InvoicePersonLease_Relation {
 		 * Once the ResultSet is filled with data, depending upon how many ResultSets are used, we 
 		 * can go ahead with the logic	
 		 */	
+		
+		
+	/*	select monthly_housing_rent,monthly_parking_rent,late_fees,incidental_charges,
+		invoice_no,payment_date,payment_method,lease_no,payment_status,payment_due,
+		damage_charges,person_id
+		from invoice_person_lease outer_table
+		where person_id = ?
+		and payment_date = (select max(payment_date) from invoice_person_lease inner_table
+		where outer_table.person_id = inner_table.person_id
+		order by payment_date desc);*/
+		
+		ResultSet rs = null;
+		Connection dbConnection = null;
+		PreparedStatement preparedStatement = null;
+		
+		try{
+			dbConnection = ConnectionUtils.getConnection();
+			
+			String selectQuery = "SELECT * FROM invoice_person_lease outer_table WHERE person_id = ? AND " ;
+			
+			selectQuery = selectQuery + " payment_date = (select max(payment_date) from invoice_person_lease inner_table " ;
+			selectQuery = selectQuery + " where outer_table.person_id = inner_table.person_id)" ;
+			
+			System.out.println("SELECT QUERY IS: "+selectQuery);
+					
+			preparedStatement = dbConnection.prepareStatement(selectQuery);
+			
+			preparedStatement.setInt(1,personId);
+			
+			rs = preparedStatement.executeQuery();
+
+			//If record exists , rs.next() will evaluate to true
+			if(rs.next())
+				{
+					System.out.print("monthly_housing_rent\t\tmonthly_parking_rent\t\tlate_fees\t\tincidental_charges\t\tinvoice_no");
+					System.out.println("\t\tpayment_date\t\tpayment_method\t\tlease_no\t\tpayment_status\t\tpayment_due\t\tdamage_charges\t\tperson_id") ;
+					
+					System.out.print(rs.getDouble("monthly_housing_rent")+"\t\t");
+					System.out.print(rs.getDouble("monthly_parking_rent")+"\t\t");
+					System.out.print(rs.getDouble("late_fees")+"\t\t");
+					System.out.print(rs.getDouble("incidental_charges")+"\t\t");
+					System.out.print(rs.getInt("invoice_no")+"\t\t");
+					System.out.print(rs.getDate("payment_date")+"\t\t");
+					System.out.print(rs.getString("payment_method")+"\t\t");
+					System.out.print(rs.getInt("lease_no")+"\t\t");
+					System.out.print(rs.getString("payment_status")+"\t\t");
+					System.out.print(rs.getDouble("payment_due")+"\t\t");
+					System.out.print(rs.getDouble("damage_charges")+"\t\t");
+					System.out.print(rs.getInt("person_id")+"\t\t");
+				}
+			
+		}catch(SQLException e1){
+			System.out.println("SQLException: "+ e1.getMessage());
+			System.out.println("VendorError: "+ e1.getErrorCode());
+		}
+		catch(Exception e3)
+		{
+			System.out.println("General Exception Case. Printing stack trace below:\n");
+			e3.printStackTrace();
+		}
+		finally{
+				try {
+			        rs.close();
+			        preparedStatement.close();
+			        dbConnection.close();
+			      } catch (SQLException e) {
+			        e.printStackTrace();
+			      }
+		}
+		
+		
 
 
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
