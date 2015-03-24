62267a262379ee115199b8daa762cf8ec5f13255
Sudhanshu Bhatt <sbhatt@ncsu.edu>
2015-03-24 14:34:59 -0400

    Adding Parking Lot code Added a relation for Student and mapping him to a parking spot Changed PSpotBelongtoPLot_Relation with availability and permit number attributes

--- UnivHousingApp/src/com/univhousing/accomodation/Bedroom.java
+++ UnivHousingApp/src/com/univhousing/accomodation/Bedroom.java
@@ -8,34 +8,5 @@ public class Bedroom {
 	public float monthlyRent;
 	public int apartmentNo;
 
-	public int getBedroomPlaceNo() {
-		return bedroomPlaceNo;
-	}
-	public void setBedroomPlaceNo(int bedroomPlaceNo) {
-		this.bedroomPlaceNo = bedroomPlaceNo;
-	}
-	public int getAccomodationId() {
-		return accomodationId;
-	}
-	public void setAccomodationId(int accomodationId) {
-		this.accomodationId = accomodationId;
-	}
-	public int getRoomNo() {
-		return roomNo;
-	}
-	public void setRoomNo(int roomNo) {
-		this.roomNo = roomNo;
-	}
-	public float getMonthlyRent() {
-		return monthlyRent;
-	}
-	public void setMonthlyRent(float monthlyRent) {
-		this.monthlyRent = monthlyRent;
-	}
-	public int getApartmentNo() {
-		return apartmentNo;
-	}
-	public void setApartmentNo(int apartmentNo) {
-		this.apartmentNo = apartmentNo;
-	}
+	
 }
