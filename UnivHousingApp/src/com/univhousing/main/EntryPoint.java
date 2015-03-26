6786d42a6cb739971167267841eb0d01075d7179
Sudhanshu Bhatt <sbhatt@ncsu.edu>
2015-03-25 23:05:28 -0400

    Reshuffled EntryPoint.java to accomodate Guest Features similar to Students, If needed commit before this will be used to go to stable mode

--- UnivHousingApp/src/com/univhousing/main/EntryPoint.java
+++ UnivHousingApp/src/com/univhousing/main/EntryPoint.java
@@ -18,6 +18,7 @@ import com.univhousing.invoice.InvoicePersonLease_Relation;
 import com.univhousing.invoice.TicketPersonStaff_Relation;
 import com.univhousing.parking.ParkingLot;
 import com.univhousing.parking.StudentParkingSpot_Relation;
+import com.univhousing.users.Guest;
 import com.univhousing.users.Person;
 import com.univhousing.users.Student;
 
@@ -83,6 +84,7 @@ public class EntryPoint {
 							if(authenticateUser(credentialObj.personId,credentialObj.password,credentialObj))
 							{
 								credentialObj.designation = studentObj.getStudentDesignation(credentialObj.personId);
+								//credentialObj.designation = "student";
 								/*P		E		N		D		I		N		G:
 								 * 
 								 * 
@@ -419,6 +421,10 @@ public class EntryPoint {
 														personObj.updateProfile(credentialObj.personId);
 															break;
 															
+														case 3:
+															System.out.println("Shwowing Back");
+															mLevelTwo = false;
+
 														default:
 															break;
 													}
@@ -496,6 +502,9 @@ public class EntryPoint {
 														personObj.updateProfile(credentialObj.personId);
 															break;
 															
+														case 3:
+															System.out.println("Shwowing Back");
+															mLevelTwo = false;
 														default:
 															break;
 													}
@@ -513,18 +522,6 @@ public class EntryPoint {
 										}
 									}
 								}
-								else if(credentialObj.designation.equalsIgnoreCase(Constants.GUEST))
-								{
-									// Do something
-									System.out.println("Enter Approval Id or Press 2 to go Back:");
-									int approvalId = inputObj.nextInt();
-									if(approvalId == 2)
-										return;
-									else
-									{
-										
-									}
-								}
 							}
 							else
 							{
@@ -533,9 +530,376 @@ public class EntryPoint {
 							// Setting true so that we can come back inside level one
 							mLevelOne = true;
 							break;
-	
+/*----------------------------------------------------------------------------------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------------------------------------------------------------------------------
+ * ----------------------------------------------------------------------------------------------------------------------------------------------------*/	
 						case 2:
-							// Guest Login code will go here.	
+
+							// Do something
+							System.out.println("Enter Approval Id or Press 2 to go Back:");
+							int approvalId = inputObj.nextInt();
+							if(approvalId == 2)
+								return;
+							else
+							{
+								Guest guestObj = new Guest();
+								credentialObj.personId = guestObj.getPersonIdFromApprovalId(approvalId);
+								//credentialObj.personId = 3;
+								// This is the first Level of Guest Hierarchy
+								while(mLevelOne)
+								{
+									// Show different menu items
+									/*Runtime.getRuntime().exec("clear");*/
+									System.out.println("\n1. Housing Option");
+									System.out.println("2. Parking Option");
+									System.out.println("3. Maintenance");
+									System.out.println("4. Profile");
+									System.out.println("5. Back\n");
+									
+									choice = inputObj.nextInt();
+									switch (choice) 
+									{
+										case 1:
+											System.out.println("Showing Housing Option");
+											// This is the second level of Guest hierarchy where he is in Housing Options
+											while(mLevelTwo)
+											{
+												/*Runtime.getRuntime().exec("clear");*/
+												System.out.println("\n1. View Invoices");
+												System.out.println("2. View Leases");
+												System.out.println("3. New Request");
+												System.out.println("4. View/Cancel Requests");
+												System.out.println("5. View Vacancy");
+												System.out.println("6. Back\n");
+												choice = inputObj.nextInt();
+												InvoicePersonLease_Relation invoicePersonObj = new InvoicePersonLease_Relation();
+												switch(choice)
+												{
+													case 1:
+														// This is the third level of Guest hierarchy where he is checking Invoices
+														while(mLevelThree)
+														{
+															/*Runtime.getRuntime().exec("clear");*/
+															System.out.println("\n1. View Current Invoice");
+															System.out.println("2. View Former Invoices");
+															System.out.println("3. Back\n");
+															choice = inputObj.nextInt();
+															
+															switch(choice)
+															{
+																case 1:
+																	invoicePersonObj.displayCurrentInvoice(credentialObj.personId);
+																	break;
+																case 2:
+																	invoicePersonObj.displayListOfInvociesForAPerson(credentialObj.personId, mInvoiceNumbers);
+																	int count = 1;
+																	/*Runtime.getRuntime().exec("clear");*/
+																	while(mLevelFour)
+																	{
+																		// Checking if the ArrayList indeed has some invoice numbers in it else move out
+																		if(!mInvoiceNumbers.isEmpty())
+																		{
+																			for(Integer item : mInvoiceNumbers)
+																			{
+																				System.out.println(count+". "+item.intValue());
+																				count++;
+																			}
+																			count = 0;
+																			System.out.println("0. Back");
+																			System.out.println("\n");
+																			choice = inputObj.nextInt();
+																			if(choice == 0)
+																			{
+																				// Taking back to Level Three
+																				mLevelFour = false;
+																			}
+																			else
+																			{
+																				invoicePersonObj.displayInvoiceDetails(credentialObj.personId, mInvoiceNumbers.get(choice-1));
+																			}
+																		}
+																		else
+																		{
+																			System.out.println("No previous invoices\n");
+																		}
+																	}
+																	
+																	// Setting true so that we can come back to Level Four
+																	mLevelFour = true;
+																	break;
+																case 3:
+																	// Taking back to Level Two
+																	mLevelThree = false;
+																	break;
+															}
+														}
+														// Setting true so that we can come back in the third level
+														mLevelThree = true;
+														break;
+													case 2:
+														/*Runtime.getRuntime().exec("clear");*/
+														System.out.println("Showing Leases");
+														while(mLevelThree)
+														{
+															System.out.println("\n1. View Current Lease");
+															System.out.println("2. View Former Leases");
+															System.out.println("3. Back\n");
+															choice = inputObj.nextInt();
+															Lease leaseObj = new Lease();
+															switch(choice)
+															{
+																case 1:
+																	leaseObj.displayCurrentLease(credentialObj.personId);
+																	break;
+																case 2:
+																	
+																	leaseObj.displayListOfLeasesForAPerson(credentialObj.personId,mLeaseNumbers);
+																	int count = 1;
+																	/*Runtime.getRuntime().exec("clear");*/
+																	while(mLevelFour)
+																	{
+																		// Checking if the ArrayList indeed has some lease numbers in it else move out
+																		if(!mLeaseNumbers.isEmpty())
+																		{
+																			for(Integer item : mLeaseNumbers)
+																			{
+																				System.out.println(count+". "+item.intValue());
+																				count++;
+																			}
+																			count = 0;
+																			System.out.println("0. Back\n");
+																			choice = inputObj.nextInt();
+																			if(choice == 0)
+																			{
+																				// Taking back to Level Three
+																				mLevelFour = false;
+																			}
+																			else
+																			{
+																				leaseObj.displayLeaseDetails(credentialObj.personId, mLeaseNumbers.get(choice-1));
+																			}
+																		}
+																		else
+																		{
+																			System.out.println("No previous leases\n");
+																		}
+																	}
+																	// Setting true so that we can come back to Level Four
+																	mLevelFour = true;
+																	break;
+																case 3:
+																	// Taking back to Level Two
+																	mLevelThree = false;
+																	break;
+															}
+														}
+														// Setting true so that we can come back in the third level
+														mLevelThree = true;															break;
+													case 3:
+														System.out.println("Generating New Request");
+														/*Runtime.getRuntime().exec("clear");*/
+														while(mLevelFour)
+														{
+															System.out.println("\n1. New Lease Request");
+															System.out.println("2. Terminate Lease Request");
+															System.out.println("3. Back\n");
+
+															LeaseRequest_Relation leaseRequestObj = new LeaseRequest_Relation();
+															TerminationRequest_Relation terminationRequestObj = new TerminationRequest_Relation();
+															choice = inputObj.nextInt();
+
+															switch(choice)
+															{
+																case 1:
+																	leaseRequestObj.generateNewLeaseRequest(credentialObj.personId);
+																	break;
+																case 2:
+																	terminationRequestObj.generateLeaseTerminationRequest(credentialObj.personId);
+																	break;
+																case 3:
+																	mLevelFour = false;
+																	break;
+															}
+														}
+														mLevelFour = true;
+														break;
+													case 4:
+														System.out.println("Showing/Viewing Cancel Requests");
+														/*Runtime.getRuntime().exec("clear");*/
+														while(mLevelFour)
+														{
+															System.out.println("\n1. View Request");	
+															System.out.println("2. Cancel Request");
+															System.out.println("3. Back\n");
+															choice = inputObj.nextInt();
+															Lease leaseObj = new Lease();
+															
+															switch(choice)
+															{
+																case 1:
+																	leaseObj.viewAllRequests(credentialObj.personId);
+																	break;
+																case 2:
+																	System.out.println("Enter the Request Number to cancel: \n");
+																	int requestNumber = inputObj.nextInt();
+																	leaseObj.cancelRequest(credentialObj.personId, requestNumber);
+																	break;
+																case 3:
+																	mLevelFour = false;
+																	break;
+															}
+														}
+														mLevelFour = true;
+														break;
+													case 5:
+														System.out.println("Showing Vacancy\n");
+
+														Lease leaseObj = new Lease();
+														leaseObj.viewAccomodationVacancies();
+														break;
+													case 6:
+														// Taking back to Level One
+														mLevelTwo = false;
+														break;
+												}
+											}
+											// Setting true so that you can come back inside Level two
+											mLevelTwo = true;
+											break;
+										case 2:
+											System.out.println("Showing Parking");
+											/*Runtime.getRuntime().exec("clear");*/
+											ParkingLot parkingLotObj = new ParkingLot();
+											while(mLevelTwo)
+											{
+												System.out.println("\n1. Request New Parking Spot");
+												System.out.println("2. View Parking Lot Information");
+												System.out.println("3. View Current Parking Spot");
+												System.out.println("4. Renew Parking Spot");
+												System.out.println("5. Return Parking Spot");
+												System.out.println("6. View Request Status");
+												System.out.println("7. Back\n");
+												
+												choice = inputObj.nextInt();
+												switch (choice) 
+												{
+													case 1:System.out.println("Showing new Parking spot\n");
+													
+														parkingLotObj.generateAParkingSpot(credentialObj.personId);
+														break;
+														
+													case 2:System.out.println("Showing View Parking Lot information\n");
+														parkingLotObj.displayInfoForParkingLots(credentialObj.personId);
+														break;
+														
+													case 3:System.out.println("Showing View Current Parking Spot\n");
+														parkingLotObj.viewCurrentParkingSpot(credentialObj.personId);
+														break;
+														
+													case 4:System.out.println("Showing Renew Parking Spot\n");
+														parkingLotObj.renewParkingSpot(credentialObj.personId);
+														break;
+														
+													case 5: System.out.println("Showing Return Parking Spot\n");
+														parkingLotObj.returnParkingSpot(credentialObj.personId);
+														break;
+
+													case 6: System.out.println("Showing View Request Status\n");
+														parkingLotObj.getRequestStatus(credentialObj.personId);
+														break;
+													case 7:
+														mLevelTwo = false;
+														break;
+
+													default: System.out.println("Invalid Choice\n");
+														break;
+												}
+											}
+											mLevelTwo = true;
+											break;
+										case 3:
+											System.out.println("Showing Maintenance");
+											/*Runtime.getRuntime().exec("clear");*/
+											TicketPersonStaff_Relation ticketObj = new TicketPersonStaff_Relation();
+											while(mLevelTwo)
+											{
+												System.out.println("\n1. New Ticket");
+												System.out.println("2. View Ticket Status");
+												System.out.println("3. Back\n");
+												choice = inputObj.nextInt();
+												
+												switch (choice) 
+												{
+													case 1:
+														System.out.println("Showing New Ticket");
+														ticketObj.raiseNewTicket(credentialObj.personId);
+														break;
+
+													case 2:
+														System.out.println("Showing View Ticket Status");
+														ticketObj.viewAllTicketStatuses(credentialObj.personId, mTicketNumbers);
+														break;
+
+													case 3:
+														System.out.println("Back\n");
+														mLevelTwo = false;
+														break;
+
+													default:
+														break;	
+												}
+											}
+											mLevelTwo = true;
+											break;
+										case 4:
+											System.out.println("Showing Profile\n");
+											Person personObj = new Person();
+
+											while(mLevelTwo)
+											{
+												System.out.println("\n1. View Profile");
+												System.out.println("2. Update Profile");
+												System.out.println("3. Back\n");
+												choice = inputObj.nextInt();
+												
+												switch (choice) 
+												{
+													case 1: System.out.println("Showing View Profile");
+														personObj.viewProfileDetails(credentialObj.personId);
+														break;
+
+													case 2: System.out.println("Showing Update Profile");
+													personObj.updateProfile(credentialObj.personId);
+														break;
+														
+													case 3:
+														System.out.println("Shwowing Back");
+														mLevelTwo = false;
+
+													default:
+														break;
+												}
+												
+											}
+											mLevelTwo = true;
+											break;
+										case 5: 
+											// Taking back to Level zero
+											mLevelOne = false;
+											break;
+										default:
+											break;
+									}
+								}
+								mLevelOne =true;
+							}
 							break;
 	
 						case 3: // Exit menu
--- UnivHousingApp/src/com/univhousing/users/Guest.java
+++ UnivHousingApp/src/com/univhousing/users/Guest.java
@@ -1,9 +1,23 @@
 package com.univhousing.users;
 
+import java.sql.ResultSet;
+
 public class Guest {
 	
 	public int approvalId;
 	public int personId;
+
+	/**
+	 * @param approvalId
+	 * @return PersonId for a given Approval Id
+	 */
+	public int getPersonIdFromApprovalId(int approvalId) {
+		/*Write SQL Query to get PersonId for the given Approval Id*/
+		int personId = 0;
+		ResultSet getPersonId = null;
+
+		return personId;
+	}
 
 
 }
