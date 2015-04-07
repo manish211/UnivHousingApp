49d38c615a68b2269d08fcde2603ba39eebf5dd0
Sudhanshu Bhatt <sbhatt@ncsu.edu>
2015-04-07 18:51:46 -0400

    Changed code to make sure Guest can have next of kin

--- UnivHousingApp/src/com/univhousing/users/Person.java
+++ UnivHousingApp/src/com/univhousing/users/Person.java
@@ -105,12 +105,12 @@ public class Person {
 				}
 				System.out.println("Category: "+ studentCategory);
 				dbConnection.close();
-				System.out.println("Now displaying your Next of Kin details:\n");
-				nokObj = new NextOfKin();
-				nokObj.getNextOfKinDetails(studentId);
-				if(!nokObj.checkIfNextOfKinPresent(studentId))
-					System.out.println("No Kin details saved, please update profile to add details");
 			}
+			System.out.println("Now displaying your Next of Kin details:\n");
+			nokObj = new NextOfKin();
+			nokObj.getNextOfKinDetails(studentId);
+			if(!nokObj.checkIfNextOfKinPresent(studentId))
+				System.out.println("No Kin details saved, please update profile to add details");
 		}
 		catch(SQLException e)
 		{
@@ -222,94 +222,92 @@ public class Person {
 		{
 			e.printStackTrace();
 		}
-		if(!guestObj.checkPersonIsGuest(personId))
+
+		System.out.println("Please enter Next Of Kin Details now: ");
+		
+		// Consuming the enter after last input 
+		inputObj.nextLine();
+		
+		System.out.println("Next of Kin First Name:");
+		String newNOKFirstName = inputObj.nextLine();
+		
+		System.out.println("Next of Kin Last Name: ");
+		String newNOKLastName = inputObj.nextLine();
+		
+		System.out.println("Next of Kin Street Name.: ");
+		String newNOKStreetNo = inputObj.nextLine();
+		
+		System.out.println("Next of Kin City: ");
+		String newNOKCity = inputObj.nextLine();
+		
+		System.out.println("Next of Kin Gender: ");
+		String newNOKGender = inputObj.nextLine();
+		
+		System.out.println("Next of Kin DOB in MM/dd/yyyy format: ");
+		String newStringNOKDOB = inputObj.nextLine();
+		java.sql.Date newNOKDOB = Utils.convertStringToSQLDateFormat(newStringNOKDOB);
+		
+		System.out.println("Next of Kin ZipCode: ");
+		int newNOKZipCode = inputObj.nextInt();
+		
+		System.out.println("Next of Kin Phone Number: ");
+		long newNOKPhone = inputObj.nextLong();
+		
+		// Consuming the enter after last input 
+		inputObj.nextLine();
+		
+		/*Write SQL Query to to update the above information for the retrieved Student ID
+		 * Students and NextOfKin(NOK) both details have to be updated.*/
+		
+		if(nokObj.checkIfNextOfKinPresent(studentId))
 		{
-			System.out.println("Please enter Next Of Kin Details now: ");
-			
-			// Consuming the enter after last input 
-			inputObj.nextLine();
-			
-			System.out.println("Next of Kin First Name:");
-			String newNOKFirstName = inputObj.nextLine();
-			
-			System.out.println("Next of Kin Last Name: ");
-			String newNOKLastName = inputObj.nextLine();
-			
-			System.out.println("Next of Kin Street Name.: ");
-			String newNOKStreetNo = inputObj.nextLine();
-			
-			System.out.println("Next of Kin City: ");
-			String newNOKCity = inputObj.nextLine();
-			
-			System.out.println("Next of Kin Gender: ");
-			String newNOKGender = inputObj.nextLine();
-			
-			System.out.println("Next of Kin DOB in MM/dd/yyyy format: ");
-			String newStringNOKDOB = inputObj.nextLine();
-			java.sql.Date newNOKDOB = Utils.convertStringToSQLDateFormat(newStringNOKDOB);
-			
-			System.out.println("Next of Kin ZipCode: ");
-			int newNOKZipCode = inputObj.nextInt();
-			
-			System.out.println("Next of Kin Phone Number: ");
-			long newNOKPhone = inputObj.nextLong();
-			
-			// Consuming the enter after last input 
-			inputObj.nextLine();
-			
-			/*Write SQL Query to to update the above information for the retrieved Student ID
-			 * Students and NextOfKin(NOK) both details have to be updated.*/
-			
-			if(nokObj.checkIfNextOfKinPresent(studentId))
+			try
 			{
-				try
-				{
-					PreparedStatement preparedStatement3 = null;
-					Connection dbConnection3 = ConnectionUtils.getConnection();
-					
-					String query3 = "UPDATE KIN_STUDENT SET first_name = ?, last_name = ?, street_name = ?," +
-							" city_name = ?, zip_code = ?, phone_number = ?, gender = ?, DOB = ? WHERE student_id = ?";
-					preparedStatement3 = dbConnection3.prepareStatement(query3);
-					preparedStatement3.setString(1, newNOKFirstName);
-					preparedStatement3.setString(2, newNOKLastName);
-					preparedStatement3.setString(3, newNOKStreetNo);
-					preparedStatement3.setString(4, newNOKCity);
-					preparedStatement3.setInt(5, newNOKZipCode);
-					preparedStatement3.setLong(6, newNOKPhone);
-					preparedStatement3.setString(7, newNOKGender);
-					preparedStatement3.setDate(8, newNOKDOB);
-					preparedStatement3.setInt(9, studentId);
-					preparedStatement3.executeUpdate();
-				}
-				catch(SQLException e)
-				{
-					e.printStackTrace();
-				}
+				PreparedStatement preparedStatement3 = null;
+				Connection dbConnection3 = ConnectionUtils.getConnection();
+				
+				String query3 = "UPDATE KIN_STUDENT SET first_name = ?, last_name = ?, street_name = ?," +
+						" city_name = ?, zip_code = ?, phone_number = ?, gender = ?, DOB = ? WHERE person_id = ?";
+				preparedStatement3 = dbConnection3.prepareStatement(query3);
+				preparedStatement3.setString(1, newNOKFirstName);
+				preparedStatement3.setString(2, newNOKLastName);
+				preparedStatement3.setString(3, newNOKStreetNo);
+				preparedStatement3.setString(4, newNOKCity);
+				preparedStatement3.setInt(5, newNOKZipCode);
+				preparedStatement3.setLong(6, newNOKPhone);
+				preparedStatement3.setString(7, newNOKGender);
+				preparedStatement3.setDate(8, newNOKDOB);
+				preparedStatement3.setInt(9, personId);
+				preparedStatement3.executeUpdate();
 			}
-			else
+			catch(SQLException e)
 			{
-				try
-				{
-					PreparedStatement preparedStatement3 = null;
-					Connection dbConnection3 = ConnectionUtils.getConnection();
-					
-					String query3 = "INSERT INTO KIN_STUDENT (student_id,first_name,last_name,street_name,city_name,zip_code,phone_number,gender,DOB) VALUES(?,?,?,?,?,?,?,?,?)";
-					preparedStatement3 = dbConnection3.prepareStatement(query3);
-					preparedStatement3.setInt(1, studentId);
-					preparedStatement3.setString(2, newNOKFirstName);
-					preparedStatement3.setString(3, newNOKLastName);
-					preparedStatement3.setString(4, newNOKStreetNo);
-					preparedStatement3.setString(5, newNOKCity);
-					preparedStatement3.setInt(6, newNOKZipCode);
-					preparedStatement3.setLong(7, newNOKPhone);
-					preparedStatement3.setString(8, newNOKGender);
-					preparedStatement3.setDate(9, newNOKDOB);
-					preparedStatement3.executeUpdate();
-				}
-				catch(SQLException e)
-				{
-					e.printStackTrace();
-				}
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			try
+			{
+				PreparedStatement preparedStatement3 = null;
+				Connection dbConnection3 = ConnectionUtils.getConnection();
+				
+				String query3 = "INSERT INTO KIN_STUDENT (person_id,first_name,last_name,street_name,city_name,zip_code,phone_number,gender,DOB) VALUES(?,?,?,?,?,?,?,?,?)";
+				preparedStatement3 = dbConnection3.prepareStatement(query3);
+				preparedStatement3.setInt(1, studentId);
+				preparedStatement3.setString(2, newNOKFirstName);
+				preparedStatement3.setString(3, newNOKLastName);
+				preparedStatement3.setString(4, newNOKStreetNo);
+				preparedStatement3.setString(5, newNOKCity);
+				preparedStatement3.setInt(6, newNOKZipCode);
+				preparedStatement3.setLong(7, newNOKPhone);
+				preparedStatement3.setString(8, newNOKGender);
+				preparedStatement3.setDate(9, newNOKDOB);
+				preparedStatement3.executeUpdate();
+			}
+			catch(SQLException e)
+			{
+				e.printStackTrace();
 			}
 		}
 	}
