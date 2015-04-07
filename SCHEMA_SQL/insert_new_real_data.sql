d5ce90076a2417ba9a6631707f80ae3412c6d39b
unknown <mrsingh@ncsu.edu>
2015-04-07 16:58:31 -0400

    added society name

---
+++ SCHEMA_SQL/insert_new_real_data.sql
@@ -0,0 +1,2 @@
+insert into person values('Harry','Potter','31 B, Privet Drive','London',27605,9189327078,
+to_date('21-07-1991','dd-mm-yyyy'),'nationality','smoker','special_needs','housing_status','study_field',9176326078);
---
+++ SCHEMA_SQL/table_updates.sql
@@ -0,0 +1,5 @@
+alter table residence_hall modify street_name varchar2(300);
+
+alter table family_apartment add society_name varchar2(300);
+
+alter table general_apartment add society_name varchar2(300);
